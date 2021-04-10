8e7f482633893e769a8ff5c8675e5fd290f5c6e2
SneezySoup <campbellcdc13@gmail.com>
2021-04-10 14:02:17 -0500

    test

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          SneezySoup's Personal Page
+          SneezySoup's Personal Page Testing
         </p>
       </header>
     </div>
