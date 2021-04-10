0d46cda920121b15c41519f282a26ee26b3397a8
SneezySoup <campbellcdc13@gmail.com>
2021-04-10 13:02:31 -0500

    initial commit

---
+++ amplifyapp/src/App.js
@@ -0,0 +1,17 @@
+import logo from './index.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          SneezySoup's Personal Page
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
